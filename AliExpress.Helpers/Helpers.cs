be54c8148d0690538bdf3a9f5a083bb27550d9ee
Kostiantyn <kost.bogomazov@gmail.com>
2018-10-09 18:20:58 +0300

    Added page login but it is not working

--- AliExpress.Helpers/Helpers.cs
+++ AliExpress.Helpers/Helpers.cs
@@ -1,8 +1,61 @@
 ﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
 
 namespace AliExpress.Helpers
 {
-    public class Class1
+    public class Helpers
     {
+
+        private IWebDriver driver;
+        private const string aliExpressURL = "https://www.aliexpress.com";
+        private const string aliExpressLogin = "skaxrfdzeajgee2w@outlook.com";
+        private const string aliExpressPassword = "qLEvZxcMVU9xqdQC";
+
+
+        public IWebElement SignInButton => driver.FindElement(
+            By.XPath("//div[@id='user-benefits']/div[@class='user-account']//span[@class='register-btn']/a[@data-role='sign-link']"));
+
+        public IWebElement LoginField => driver.FindElement(By.Id("fm-login-id"));
+        public IWebElement PasswordField => driver.FindElement(By.Id("fm-login-password"));
+        public IWebElement LoginSubmitButton => driver.FindElement(By.Id("fm-login-submit"));
+
+        // public IWebElement AdsCloseButton => driver.FindElement(By.XPath("//a[@data-role='layer-close']"));
+        public IWebElement AdsCloseButton => driver.FindElement(By.XPath("/html/body/div[7]/div/div/a"));
+        public IWebElement GoToGlobalSiteLink => driver.FindElement(By.LinkText("Go to Global Site (English)"));
+
+
+
+        public Helpers(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void NavigateToAliExpressHomepage()
+        {
+            driver.Navigate().GoToUrl(aliExpressURL);
+        }
+
+        public void LoginToAliExpress()
+        {
+
+            // TODO: try to set cookie
+            // TODO: try to find those elements using class attribute
+
+            // <a href="javascript:;" class="close-layer" data-role="layer-close">x</a>
+            // <a data-role="goto-globalsite" class="link-goto-globalsite notranslate" rel="nofollow" href="http://www.aliexpress.com/">Go to Global Site (English)</a>
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementToBeClickable(AdsCloseButton));
+            Thread.Sleep(5000);
+
+            AdsCloseButton.Click();
+            GoToGlobalSiteLink.Click();
+            SignInButton.Click();
+            LoginField.SendKeys(aliExpressLogin);
+            PasswordField.SendKeys(aliExpressPassword);
+            LoginSubmitButton.Click();
+        }
     }
 }
--- AliExpress.Tests/UnitTest1.cs
+++ AliExpress.Tests/UnitTest1.cs
@@ -1,18 +1,37 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using AliExpress.Helpers;
+using System.Threading;
 
 namespace Tests
 {
+    [TestFixture]
     public class Tests
     {
+
+        private IWebDriver driver;
+
         [SetUp]
         public void Setup()
         {
+
+            ChromeOptions options = new ChromeOptions();
+            options.PageLoadStrategy = PageLoadStrategy.None; // PageLoadStrategy.Eager not supported by Chrome
+            driver = new ChromeDriver("/home/kbogomazov/dotnet_src/lib", options);
+            driver.Manage().Window.Maximize();
+
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            Helpers helper = new Helpers(driver);
+            helper.NavigateToAliExpressHomepage();
+            helper.LoginToAliExpress();
+            Thread.Sleep(5000);
+
         }
     }
 }
