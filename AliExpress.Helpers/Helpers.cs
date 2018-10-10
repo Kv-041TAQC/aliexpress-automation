031294a8064b25879c946916016bf0517d89cc99
Kostiantyn <kost.bogomazov@gmail.com>
2018-10-10 18:32:50 +0300

    Rough fix of Sign In in Helpers class

--- AliExpress.Helpers/Helpers.cs
+++ AliExpress.Helpers/Helpers.cs
@@ -14,6 +14,8 @@ namespace AliExpress.Helpers
         private const string aliExpressLogin = "skaxrfdzeajgee2w@outlook.com";
         private const string aliExpressPassword = "qLEvZxcMVU9xqdQC";
 
+        private const string aliExpressLoginFormId = "alibaba-login-box";
+
 
         public IWebElement SignInButton => driver.FindElement(
             By.XPath("//div[@id='user-benefits']/div[@class='user-account']//span[@class='register-btn']/a[@data-role='sign-link']"));
@@ -22,8 +24,8 @@ namespace AliExpress.Helpers
         public IWebElement PasswordField => driver.FindElement(By.Id("fm-login-password"));
         public IWebElement LoginSubmitButton => driver.FindElement(By.Id("fm-login-submit"));
 
-        // public IWebElement AdsCloseButton => driver.FindElement(By.XPath("//a[@data-role='layer-close']"));
-        public IWebElement AdsCloseButton => driver.FindElement(By.XPath("/html/body/div[7]/div/div/a"));
+        public IWebElement AdsCloseButton => driver.FindElement(By.XPath("//a[@data-role='layer-close']"));
+        // public IWebElement AdsCloseButton => driver.FindElement(By.XPath("/html/body/div[7]/div/div/a"));
         public IWebElement GoToGlobalSiteLink => driver.FindElement(By.LinkText("Go to Global Site (English)"));
 
 
@@ -38,6 +40,14 @@ namespace AliExpress.Helpers
             driver.Navigate().GoToUrl(aliExpressURL);
         }
 
+        public void WaitForElement(IWebElement element)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementToBeClickable(element));
+            Console.WriteLine($"Waiting for {element.GetAttribute("class")} to appear.");
+            Thread.Sleep(15000);
+        }
+
         public void LoginToAliExpress()
         {
 
@@ -46,16 +56,44 @@ namespace AliExpress.Helpers
 
             // <a href="javascript:;" class="close-layer" data-role="layer-close">x</a>
             // <a data-role="goto-globalsite" class="link-goto-globalsite notranslate" rel="nofollow" href="http://www.aliexpress.com/">Go to Global Site (English)</a>
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            wait.Until(ExpectedConditions.ElementToBeClickable(AdsCloseButton));
-            Thread.Sleep(5000);
-
+            
+            WaitForElement(AdsCloseButton);
             AdsCloseButton.Click();
+            WaitForElement(GoToGlobalSiteLink);
             GoToGlobalSiteLink.Click();
+            WaitForElement(SignInButton);
             SignInButton.Click();
+            WaitForElement(driver.FindElement(By.Id(aliExpressLoginFormId)));
+            driver.SwitchTo().Frame(driver.FindElement(By.Id(aliExpressLoginFormId))); // change to lambda expression
+            WaitForElement(LoginField);
             LoginField.SendKeys(aliExpressLogin);
+            WaitForElement(PasswordField);
             PasswordField.SendKeys(aliExpressPassword);
+            WaitForElement(LoginSubmitButton);
             LoginSubmitButton.Click();
+
+            // TODO: click my orders link
+            // TODO: click shipping address menu
         }
+
+        public void OpenMyOrdersPage()
+        {
+
+        }
+
+        public void OpenShipmentAddress()
+        {
+
+        }
+
+        // TODO: move all this stuff into:
+        // Homepage
+        // MyAliExpress page
+        // which switches into My Orders and other options
+        // in helper class should be waiters, ads/popup windows checking/closing
+        // make normal class hierarchy
+        // work with ajax / js
+
+
     }
 }
--- AliExpress.Tests/UnitTest1.cs
+++ AliExpress.Tests/UnitTest1.cs
@@ -4,6 +4,8 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
 using AliExpress.Helpers;
 using System.Threading;
+using System;
+using OpenQA.Selenium.Support.UI;
 
 namespace Tests
 {
@@ -21,6 +23,7 @@ namespace Tests
             options.PageLoadStrategy = PageLoadStrategy.None; // PageLoadStrategy.Eager not supported by Chrome
             driver = new ChromeDriver("/home/kbogomazov/dotnet_src/lib", options);
             driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 
         }
 
@@ -29,8 +32,9 @@ namespace Tests
         {
             Helpers helper = new Helpers(driver);
             helper.NavigateToAliExpressHomepage();
+            Thread.Sleep(5000); // why this works only here
             helper.LoginToAliExpress();
-            Thread.Sleep(5000);
+            
 
         }
     }
