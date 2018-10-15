6d988ccbab1adc03f60b3a2aa6ab7bd3ecf919ca
Vasia <fgzf.fgzf.1999@mail.ru>
2018-10-15 21:06:24 +0300

    Add VasylPages clases and LocalTest

---
+++ AliExpressAutomation/Test/RegistrationTests/LocalTestbyVasyl.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using Pages.VasylPages;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Tests.RegistrationTests
+{
+    [TestFixture]
+    public class LocalTestbyVasyl
+    {
+        [Test]
+        public void LocalTest()
+        {
+            using (ChromeDriver dr = new ChromeDriver(Directory.GetCurrentDirectory()))
+            {
+                var mainPage = new MyMainPage(dr);
+                var searchPage = mainPage.NextPage();
+                var goodsPage = searchPage.NextPage();
+                var cartPage = goodsPage.NextPage();
+                var franceMainPage = cartPage.Nextpage();
+                var franceSearchPage = franceMainPage.NextPage();
+                var franceGoodsPage = franceSearchPage.NextPage();
+                cartPage = franceGoodsPage.NextPage();
+                cartPage.RemoveCart();
+            }
+        }
+    }
+}
