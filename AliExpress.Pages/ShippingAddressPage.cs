bed5ec47c7ac0e771dfccf63a4b0354dda9ce83e
Kostiantyn Bogomazov <kost.bogomazov@gmail.com>
2018-10-16 13:52:38 +0300

    Added more element locators and shipping address form filling.

--- AliExpress.Pages/ShippingAddressPage.cs
+++ AliExpress.Pages/ShippingAddressPage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 
 
@@ -8,10 +9,12 @@ namespace AliExpress.Pages
     public class ShippingAddressPage
     {
         private IWebDriver driver;
+
         public ShippingAddressPage(IWebDriver driver)
         {
             this.driver = driver;
         }
+
         #region Page Element Locators
         // 10 elements
 
@@ -19,7 +22,7 @@ namespace AliExpress.Pages
         public IWebElement ContactPersonInputField => driver.FindElement(By.Name("contactPerson"));
 
         // <select name="country" class="ui-textfield ui-textfield-system sa-country" data-spm-anchor-id="a2g0s.8850659.0.i3.3d4a4c4dw7y1kW"><</option></select>
-        public IWebElement CountryDropDownList => driver.FindElement(By.Name("country"));
+        public SelectElement CountryDropDownList => new SelectElement(driver.FindElement(By.Name("country")));
 
         // <input name="address" class="ui-textfield ui-textfield-system js-input-field" type="text" placeholder="Street address" maxlength="256">
         public IWebElement AddressInputField => driver.FindElement(By.Name("address"));
@@ -30,11 +33,14 @@ namespace AliExpress.Pages
         // TODO: use better Xpath here, or something else
         // <select class="ui-textfield ui-textfield-system" style="display: inline-block;"></select>
         // //div[@id='address-main']/div[@class='sa-form']/div[@class='row sa-form-group sa-province-group']/div/select[@class='ui-textfield ui-textfield-system']
-        public IWebElement StateProvinceDropDownList => driver.FindElement(
-            By.XPath("//div[@class='sa-form']/div[@class='row sa-form-group sa-province-group']/div/select"));
+        public SelectElement StateProvinceDropDownList => new SelectElement(driver.FindElement(
+            By.XPath("//div[@class='sa-form']/div[@class='row sa-form-group sa-province-group']/div/select")));
 
         // <input name="city" type="text" class="ui-textfield ui-textfield-system sa-error-field" maxlength="64" data-spm-anchor-id="a2g0s.8850659.0.i1.46b34c4dhEovLL" style="display: inline-block;">
-        public IWebElement CityInputField => driver.FindElement(By.Name("city"));
+        // <select class="ui-textfield ui-textfield-system sa-error-field" style="display: none;" data-spm-anchor-id="a2g0s.8850659.0.i8.68664c4dx1X0Dr">
+        //div[@class='row sa-form-group sa-city-group']/div/select
+        // public IWebElement CityInputField => driver.FindElement(By.Name("city"));
+        public SelectElement CityDropDownList => new SelectElement(driver.FindElement(By.XPath("//div[@class='row sa-form-group sa-city-group']/div/select")));
 
         // <input name="zip" class="ui-textfield ui-textfield-system sa-zip js-input-field" type="text" maxlength="10">
         public IWebElement ZipInputField => driver.FindElement(By.Name("zip"));
@@ -52,6 +58,22 @@ namespace AliExpress.Pages
         // //div[@id='address-main']/div[@class='sa-form']//input[@name='isDefault']
         public IWebElement SetAsDefaultCheckBox => driver.FindElement(By.XPath("//div[@class='sa-form']//input[@name='isDefault']"));
 
+        // TODO: add success icon checks (1.input 2.click on form 3.check for icon)
+        // contact name  //div[@id='address-main']/div[@class='sa-form']/div[1]//div[@class='sa-success-icon']
+        // country/region success icon //div[@id='address-main']/div[@class='sa-form']/div[2]//div[@class='sa-success-icon']
+        // street address //div[@id='address-main']/div[@class='sa-form']/div[3]/ul[@class='sa-form-control sa-street-wrapper']/li[1]//div[@class='sa-success-icon']
+        // apt //div[@id='address-main']/div[@class='sa-form']/div[3]/ul[@class='sa-form-control sa-street-wrapper']/li[2]//div[@class='sa-success-icon']
+        // state/province/region //div[@id='address-main']/div[@class='sa-form']/div[@class='row sa-form-group sa-province-group']//div[@class='sa-success-icon']
+        // city //div[@id='address-main']/div[@class='sa-form']/div[5]//div[@class='sa-success-icon']
+        // zip //div[@id='address-main']/div[@class='sa-form']/div[6]//div[@class='sa-success-icon']
+        // mobile //div[@id='address-main']/div[@class='sa-form']/div[7]//div[@class='sa-success-icon']
+
+        // TODO: add error checks
+        
+
+
+
+
         // <a href="javascript:;" class="ui-button ui-button-primary ui-button-medium sa-confirm" style="">Save</a>
         public IWebElement SaveButton => driver.FindElement(By.LinkText("Save"));
 
@@ -67,7 +89,21 @@ namespace AliExpress.Pages
 
         public void FillShippingAddressForm()
         {
-
+            ContactPersonInputField.SendKeys("John Doe");
+            CountryDropDownList.SelectByText("United States");
+            AddressInputField.SendKeys("10 Test Ave");
+            ApartmentInputField.SendKeys("15");
+            StateProvinceDropDownList.SelectByText("New York");
+            CityDropDownList.SelectByText("New york");
+            ZipInputField.SendKeys("11221");
+            PhoneCountryInputField.Clear();
+            PhoneCountryInputField.SendKeys("+1");
+            MobileNumberInputField.SendKeys("5417543111");
+            SaveButton.Click();
         }
+
+        // TODO: add method isAddressPresent
+        // make address as struct/class
+        // add error locators and error checking methods (or how to check for errors in test)
     }
 }
--- AliExpress.Tests/UnitTest1.cs
+++ AliExpress.Tests/UnitTest1.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
 using AliExpress.Helpers;
+using AliExpress.Pages;
 using System.Threading;
 using System;
 using OpenQA.Selenium.Support.UI;
@@ -21,7 +22,8 @@ namespace Tests
 
             ChromeOptions options = new ChromeOptions();
             options.PageLoadStrategy = PageLoadStrategy.None; // PageLoadStrategy.Eager not supported by Chrome
-            driver = new ChromeDriver("/home/kbogomazov/dotnet_src/lib", options);
+            // driver = new ChromeDriver("/home/kbogomazov/dotnet_src/lib", options);
+            driver = new ChromeDriver(@"F:\src\qa\qa_automation_lib", options);
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 
@@ -34,7 +36,8 @@ namespace Tests
             helper.NavigateToAliExpressHomepage();
             Thread.Sleep(5000); // why this works only here
             helper.LoginToAliExpress();
-            
+            ShippingAddressPage shippingAddressPage = new ShippingAddressPage(driver);
+            shippingAddressPage.FillShippingAddressForm();
 
         }
     }
