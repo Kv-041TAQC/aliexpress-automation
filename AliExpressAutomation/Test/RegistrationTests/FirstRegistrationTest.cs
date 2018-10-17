9342df39e44b6c4471fc9ba234deeb121ba42071
Yura_Sen <senin.yura99@gmail.com>
2018-10-17 18:23:40 +0300

    Update FirstRegistrationTest.cs

--- AliExpressAutomation/Test/RegistrationTests/FirstRegistrationTest.cs
+++ AliExpressAutomation/Test/RegistrationTests/FirstRegistrationTest.cs
@@ -12,16 +12,18 @@ namespace Test
     {
         ChromeOptions options = new ChromeOptions();
         public static int[] arr = new int[] { 0, 1, 2 };
-        [Test, TestCaseSource("arr")]
+
+        [Test,TestCaseSource("arr")]
         public void FirstSearchStringPositiveTest(int index)
         {
             options.PageLoadStrategy = PageLoadStrategy.None;
-            using (ChromeDriver dr = new ChromeDriver(Directory.GetCurrentDirectory(), options))
+            using (ChromeDriver dr = new ChromeDriver(Directory.GetCurrentDirectory(),options))
             {
                 var searchtest = new SearchStringPage(dr);
                 searchtest.RunPostiveTest(index);
             }
         }
+
     }
     [TestFixture]
     [Parallelizable]
@@ -29,11 +31,12 @@ namespace Test
     {
         ChromeOptions options = new ChromeOptions();
         static int[] arr = new int[] { 0, 1, 2 };
+
         [Test, TestCaseSource("arr")]
         public void FirstSearchStringNegativeTest(int index)
         {
             options.PageLoadStrategy = PageLoadStrategy.None;
-            using (ChromeDriver dr = new ChromeDriver(Directory.GetCurrentDirectory(), options))
+            using (ChromeDriver dr = new ChromeDriver(Directory.GetCurrentDirectory(),options))
             {
                 var searchtest = new SearchStringPage(dr);
                 searchtest.RunNegativeTest(index);
@@ -41,3 +44,4 @@ namespace Test
         }
     }
 }
+
