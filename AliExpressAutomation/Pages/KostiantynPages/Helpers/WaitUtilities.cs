9875be69fc0df18318d9eea488221a58650f42fc
Kostiantyn Bogomazov <kost.bogomazov@gmail.com>
2018-10-19 12:41:36 +0300

    Cleanup

--- AliExpressAutomation/Pages/KostiantynPages/Helpers/WaitUtilities.cs
+++ AliExpressAutomation/Pages/KostiantynPages/Helpers/WaitUtilities.cs
@@ -8,28 +8,11 @@ namespace AliExpress.Helpers
 {
     public class WaitUtilities
     {
-        /* 
-                public static void WaitForElement(IWebDriver driver, Func<IWebDriver, IWebElement>, double timeoutSeconds)
-                {      
-                    WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutSeconds));
-                    wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
-                    // wait.Until(driver1 => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
-                    wait.Until(condition);
-                    // Console.WriteLine($"Waiting for {element.GetAttribute("class")} to appear.");
-                }
-        */
-        public static void WaitUntilPageLoaded(IWebDriver driver)
+        public static string JsDocumentLoadState(IWebDriver driver)
         {
             IJavaScriptExecutor jsExec = (IJavaScriptExecutor)driver;
-            string documentLoadState;
-
-            do
-            {
-                documentLoadState = (string)jsExec.ExecuteScript("return document.readyState");
-                Thread.Sleep(TimeSpan.FromMilliseconds(100));
-            }
-            while (documentLoadState.Equals("interactive "));
-
+            string documentLoadState = jsExec.ExecuteScript("return document.readyState").ToString();
+            return documentLoadState;
         }
 
     }
