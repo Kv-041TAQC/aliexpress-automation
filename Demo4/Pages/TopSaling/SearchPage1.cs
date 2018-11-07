a47ccf84134c5ece6c5bbd276431709f91ed4b0c
Yura Senin <senin.yura99@gmail.com>
2018-11-07 18:00:32 +0200

    Changed XPath to CssSelector in SearchPage

--- Demo4/Pages/TopSaling/SearchPage1.cs
+++ Demo4/Pages/TopSaling/SearchPage1.cs
@@ -12,34 +12,34 @@ namespace Pages.TopSaling
         #region Constants
         protected int minimumForTopSaling = 1000;
         protected int maxPhonesOnPages = 48;
+        private string cssFirstGoodName = "#list-items > li.list-item.list-item-first.util-clearfix.list-item-180 > div.right-block.util-clearfix > div > div.detail > h3 > a > span";
+        private string cssFirstGoodPrice = "#list-items > li.list-item.list-item-first.util-clearfix.list-item-180 > div.right-block.util-clearfix > div > div.info.infoprice > span > span.value";
+        private string cssFirstGoodOrders = "#list-items > li.list-item.list-item-first.util-clearfix.list-item-180 > div.right-block.util-clearfix > div > div.info.infoprice > div.rate-history > span.order-num > a > em";
         private string cssButtonSecondPage = "#pagination-bottom > div.ui-pagination-navi.util-left > span";
-        private string firstHalfXpath = "//*[@id='list-items']/li[";
+        private string firstHalfCss = "# list-items > li:nth-child(";
+
+
 
         //private string firstHalfXpathPrice = "//*[@id='list-items']/li[";
-        private string secondHalfXpathPrice = "]/div[1]/div/div[2]/span/span[1]";
+        private string secondHalfCssPrice = ") > div.right-block.util-clearfix > div > div.info.infoprice > span > span.value";
 
         //private string firstHalfXpathName = "//*[@id='list-items']/li[";
-        private string secondHalfXpathName = "]/div[1]/div/div[1]/h3/a/span";
+        private string secondHalfCssName = ") > div.right-block.util-clearfix > div > div.detail > h3 > a > span";
 
         //private string firstHalfXpathOrders = "//*[@id='list-items']/li[";
-        private string secondHalfXpathOrders = "]/div[1]/div/div[2]/div[2]/span[3]/a/em";
+        private string secondHalfCssOrders = ") > div.right-block.util-clearfix > div > div.info.infoprice > div.rate-history > span.order-num > a > em";
 
         #endregion
 
         #region WebElements
         protected IWebElement SearchWebElements(string xpath)
         {
-            return driver.FindElement(By.XPath(xpath));
+            return driver.FindElement(By.CssSelector(xpath));
         }
         private IWebElement ButtonSecondPage => driver.FindElement(By.CssSelector(cssButtonSecondPage));
 
         #endregion
-//# list-items > li.list-item.list-item-first.util-clearfix.list-item-180 > div.right-block.util-clearfix > div > div.info.infoprice > div.rate-history > span.order-num > a > em
-//# list-items > li.list-item.list-item-first.util-clearfix.list-item-180 > div.right-block.util-clearfix > div > div.info.infoprice > div.rate-history > span.order-num > a > em
-//# list-items > li:nth-child(2) > div.right-block.util-clearfix > div > div.info.infoprice > div.rate-history > span.order-num > a > em
-//# list-items > li:nth-child(14) > div.right-block.util-clearfix > div > div.info.infoprice > div.rate-history > span.order-num > a > em
-//# list-items > li:nth-child(35) > div.right-block.util-clearfix > div > div.info.infoprice > div.rate-history > span.order-num > a > em
-//# list-items > li:nth-child(48) > div.right-block.util-clearfix > div > div.info.infoprice > div.rate-history > span.order-num > a > em
+
         #region Methods
         protected int ParseOrders(string notParseOrder)
         {
@@ -57,16 +57,23 @@ namespace Pages.TopSaling
 
         protected void FindAndWriteTopPhones()
         {
-            for (int i = 1; i <= maxPhonesOnPages; i++)
+                var firstNameElement = SearchWebElements(cssFirstGoodName);
+                var firstPriceElement = SearchWebElements(cssFirstGoodPrice);
+                var firstOrdersElement = SearchWebElements(cssFirstGoodOrders);
+                phones[0].name = firstNameElement.Text;
+                phones[0].price = firstPriceElement.Text;
+                phones[0].orders = ParseOrders(firstOrdersElement.Text);
+
+            for (int i = 2; i <= maxPhonesOnPages; i++)
             {
-                string ordersXpath = firstHalfXpath + i + secondHalfXpathOrders;
+                string ordersXpath = firstHalfCss + i + secondHalfCssOrders;
                 var ordersElement = SearchWebElements(ordersXpath);
                 int orders = ParseOrders(ordersElement.Text);
                 if (orders > minimumForTopSaling)
                 {
-                    string nameXpath = firstHalfXpath + i + secondHalfXpathName;
+                    string nameXpath = firstHalfCss + i + secondHalfCssName;
                     var nameElement = SearchWebElements(nameXpath);
-                    string priceXpath = firstHalfXpath + i + secondHalfXpathPrice;
+                    string priceXpath = firstHalfCss + i + secondHalfCssPrice;
                     var priceElement = SearchWebElements(priceXpath);
                     phones[countPhone].name = nameElement.Text;
                     phones[countPhone].price = priceElement.Text;
