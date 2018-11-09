49a829e6ff0ab0af90ecfba28bf7453a2a57528e
AnnaPuchko <annapuchko@gmail.com>
2018-11-09 18:51:22 +0200

    added RemoteWebDriver
    added RemoteWebDriver

--- Demo4/Test/Test/BasicTest.cs
+++ Demo4/Test/Test/BasicTest.cs
@@ -1,18 +1,39 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using NUnit.Framework;
+using Allure.Commons;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
 using NUnit.Framework.Interfaces;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using Pages.DatabaseStuff;
 
 namespace Tests.Test
 {
     [TestFixture]
+    [AllureNUnit]
     public class BasicTest
     {
+
+        [OneTimeSetUp]
+        public void CleanupResultDirectory()
+        {
+            AllureExtensions.WrapSetUpTearDownParams(() => { AllureLifecycle.Instance.CleanupResultDirectory(); },
+                "Cleanup Allure Results Directory");
+        }
+
+        
         [Test]
+        [AllureTag("NUnit","Debug")]
+        [AllureIssue("GitHub#1", "https://github.com/unickq/allure-nunit")]
+        [AllureFeature("Core")]
         public void Test()
         {
-            using (ChromeDriver dr = new ChromeDriver(Directory.GetCurrentDirectory()))
+            ChromeOptions chrome = new ChromeOptions();
+            // options.AddExtensions("/full/path/to/extension.crx");
+
+            using (RemoteWebDriver dr = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), chrome))
             {
                 dr.Navigate().GoToUrl("http://www.google.com");
                 MsSql ms = new MsSql();
