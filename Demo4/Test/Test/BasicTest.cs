a195eb4c30cfd2b5a08b670c6d5c021d8901c81f
llasapg <evgengavrasienko@gmail.com>
2018-11-10 22:54:46 +0200

    Fixed all

--- Demo4/Test/Test/BasicTest.cs
+++ Demo4/Test/Test/BasicTest.cs
@@ -2,11 +2,23 @@
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using OpenQA.Selenium.Chrome;
+using Pages.DatabaseStuff;
 
 namespace Tests.Test
 {
+    [TestFixture]
     public class BasicTest
     {
-        
+        [Test]
+        public void TestTest()
+        {
+            using (ChromeDriver dr = new ChromeDriver(Directory.GetCurrentDirectory()))
+            {
+                dr.Navigate().GoToUrl("http://www.google.com");
+                MsSql ms = new MsSql();
+                ms.Add(new AliGoods() {Name = "Suka_Rabotaet",Price = 9999,Orders = 3 });
+                ms.CloseAllConnections();
+            }
+        }
     }
 }
