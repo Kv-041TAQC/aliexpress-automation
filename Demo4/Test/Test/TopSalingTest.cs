e9ce3f293aa057780dd26505fd68095d8e4fee62
AnnaPuchko <annapuchko@gmail.com>
2018-11-12 20:18:04 +0200

    merge fixed

--- Demo4/Test/Test/TopSalingTest.cs
+++ Demo4/Test/Test/TopSalingTest.cs
@@ -1,26 +1,38 @@
-using NUnit.Framework;
-using System.IO;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium;
-using System.Threading;
-using OpenQA.Selenium.Support.UI;
 using System;
-using Pages.TopSaling;
-using Pages.DatabaseStuff;
-using Pages;
-using Pages.HelpClass;
-using NUnit.Framework.Interfaces;
 using System.Collections;
+using Allure.Commons;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using Pages;
+using Pages.DatabaseStuff;
+using Pages.HelpClass;
+using Pages.TopSaling;
 
 namespace Test
 {
     [TestFixture]
+    [AllureNUnit]
     /// <summary>
     /// <para>Test of *Find Top Phones and add it to data base* </para>
     /// </summary>
     public class TopSallingTest
     {
+        [OneTimeSetUp]
+        public void CleanupResultDirectory()
+        {
+            AllureExtensions.WrapSetUpTearDownParams(() => { AllureLifecycle.Instance.CleanupResultDirectory(); },
+                "Cleanup Allure Results Directory");
+        }
+        
         [Test]
+        [AllureTag("NUnit","Debug")]
+        [AllureIssue("GitHub#1", "https://github.com/Kv-041TAQC/aliexpress-automation")]
+        [AllureFeature("Core")]
         /// <summary>
         /// <para>Top saling test</para>
         /// </summary>
@@ -28,7 +40,7 @@ namespace Test
         {
             ChromeOptions options = new ChromeOptions();
             options.PageLoadStrategy = PageLoadStrategy.None;
-            using (ChromeDriver dr = new ChromeDriver(Directory.GetCurrentDirectory(), options))
+            using (RemoteWebDriver dr = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), options))
             {
                 SuperPage.phones = new Phone[144];
                 HelpClass helper = new HelpClass();
